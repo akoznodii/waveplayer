2efc6da1198e3e14dfa6e4e036fc103099149752
Alexander Koznodii <alexander.koznodii@hotmail.com>
2015-01-21 15:54:33 +0200

    Add HttpWebRequestHelper class

---
+++ WavePlayer/Network/FileInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WavePlayer.Network
+{
+    internal class FileInfo
+    {
+        public long ContentLength { get; set; }
+
+        public bool AcceptRanges { get; set; }
+
+        public Uri Location { get; set; }
+    }
+}
---
+++ WavePlayer/Network/HttpWebRequestHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace WavePlayer.Network
+{
+    internal static class HttpWebRequestHelper
+    {
+        private const int DefaultTimeout = 30000;
+        
+        public static FileInfo GetRemoteFileInfo(Uri location)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(location);
+
+            var response = request.GetResponse();
+
+            return new FileInfo()
+            {
+                Location = location,
+                ContentLength = response.ContentLength,
+                AcceptRanges = String.Compare(response.Headers["Accept-Ranges"], "bytes", StringComparison.OrdinalIgnoreCase) == 0
+            };
+        }
+
+        public static Stream CreateStream(Uri location, long startPosition, long endPosition)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(location);
+
+            request.Timeout = DefaultTimeout;
+
+            if (endPosition != 0)
+            {
+                request.AddRange(startPosition, endPosition);
+            }
+            else
+            {
+                request.AddRange(startPosition);
+            }
+
+            var response = request.GetResponse();
+
+            return response.GetResponseStream();
+        }
+    }
+}
